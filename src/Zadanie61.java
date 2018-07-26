9a35b984b41c39982da0c4ca08e746f8a5b8e1b3
PC <p.cz@outlook.com>
2018-07-26 16:25:28 +0200

    Zadanie 6.1

--- src/Zadanie61.java
+++ src/Zadanie61.java
@@ -1,31 +1,22 @@
+import java.util.Arrays;
 import java.util.Random;
-//zadanie nieukończone
+
 public class Zadanie61 {
 
     public static void main(String[] args) {
 
-        int size = 10;
         int[] numbers = new int[10];
+        String[] stringNumbers = new String[10];
         Random rand = new Random();
 
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = rand.nextInt(100);
-            size++;
+            stringNumbers[(stringNumbers.length - i - 1)] = Integer.toString(numbers[i]);
         }
-        size--;
-        int[] numbers2 = new int[size];
-
-        for (int tablica20:numbers2) {
-            System.out.print(tablica20 + " ");
-        }
-
-        for (int i = 0; i < numbers.length; i++) {
-            numbers2[size] = numbers[i];
-            size--;
-        }
-        for (int numery : numbers2) {
-            System.out.print(numery + " ");
-
-        }
+//        for (int numery:numbers) {
+//            System.out.print(" " + numery);
+//        }
+        System.out.print(Arrays.toString(numbers));
+        System.out.print(Arrays.toString(stringNumbers));
     }
 }
